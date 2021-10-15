c76623bd72cb1dcccc705249d2f349b76d6d9d66
sachin kannaujiya <sachinkannaujiya98@gmail.com>
2021-10-15 12:33:37 +0530

    to do app using react js by sachin kannujiy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState('');
+  const [item, setItems] = useState([]);
+
+  const handleSubmit = () => {
+    if (!data) {
+
+    } else {
+      setItems([data, ...item])
+      setData('')
+    }
+
+  }
+
+  // delete 
+  const deleteItem =(id)=>{
+       const updateItems = item.filter((val, ind)=>{
+          return ind!==id;
+          console.log(val);
+        });
+        setItems(updateItems);
+  }
+  // const date=new Date().toString();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main_div">
+        <h1>Developed By Sachin Using ReactJs</h1>
+        <div className="todo">
+          <h2 className="title">TO DO</h2>
+          <div className="heading_todo">
+            <div className="add_todo">
+              <input type="text" placeholder="Add To-do..."
+                onChange={(e) => setData(e.target.value)}
+                value={data} />
+              <button onClick={handleSubmit}>+</button>
+            </div>
+          </div>
+          {/* added item */}
+
+
+
+          {/* item? */}
+          {
+            item.map((val, ind) => {
+              return (
+                <>
+                  <div className="items">
+                    <ol className="items_list">
+                      <div className="list">
+                        <li className="lists" key={ind}>{val}</li> 
+                      </div>
+                      <div className="list_action">
+                        <button type="submit"
+                        onClick={()=>deleteItem(ind)}
+                        ><span>&#128465;&#65039;</span>
+                        </button>
+                        {/* <button type="submit">edit</button>
+                        <button type="submit">edit</button> */}
+                      </div>
+                    </ol>
+                  </div>
+                </>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
